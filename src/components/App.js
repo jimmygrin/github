f9c78e28f42f44772e719c889f565b004edb0209
Jimmy Lin <jimmydlin@gmail.com>
2019-11-08 07:08:05 -0800

    v3

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { useGithub } from '../redux/ducks/github'
 import User from './User'
 import Repos from './Repos'
 import Main from './Main'
+import 'font-awesome/css/font-awesome.min.css'
 
 
 
@@ -23,17 +24,35 @@ function App() {
   const { user, repos } = useGithub('jimmygrin')
   
   return (
-     
+     <>
+     <div id="topcontainer">
+     <div id="leftmenu">
+     <i class="fa fa-github"></i>
+      <input type="text" placeholder="Search or jump to.."/>
+      <p>Pull requests</p>
+      <p>Issues</p>
+      <p>Marketplace</p>
+      <p>Explore</p>
+       </div> 
+
+      <div id="rightmenu">
+      <i class="fa fa-bell"></i>
+      <i class="fa fa-plus"></i>
+      <i class="fa fa-sort-down"></i>
+      {/* <img src={props.avatar_url} /> */}
+      </div> 
+      
+      </div>
       <div className="container">
       
       <div id="main">
-
+        
         <User {...user}/>
         <Repos repos={repos} {...user} />
       
       </div>
       </div>
-  
+  </>
   )
 }
 
--- src/components/User.js
+++ src/components/User.js
@@ -9,6 +9,9 @@ export default function (props) {
                 <h1>{props.name}</h1>
                 <h2>{props.login}</h2>
                 <p>Follow</p>
+                <div><i className="fa fa-globe"></i>{props.location}</div>
+                <div><i className="fa fa-envelope"></i>jimmydlin@gmail.com</div>
+
         </div>
     )
 
